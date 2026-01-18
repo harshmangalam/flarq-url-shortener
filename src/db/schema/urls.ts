f89c02ecbdc8a8a770e5f1447672fbcea2911c8f
harshmangalam <harshdev8218@gmail.com>
2026-01-19 01:49:26 +0530

    added drizzle

---
+++ src/db/schema/urls.ts
@@ -0,0 +1,11 @@
+import { sql } from "drizzle-orm";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+
+export const urlsTable = sqliteTable("urls", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  longUrl: text("long_url").notNull(),
+  shortCode: text("short_code").unique(),
+  createdAt: integer("created_at", { mode: "timestamp_ms" })
+    .notNull()
+    .default(sql`(unixepoch() * 1000)`),
+});
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
 import { Hono } from "hono";
 import { HTTP_STATUS } from "./utils/http-status";
+import { drizzle, DrizzleD1Database } from "drizzle-orm/d1";
+import { urlsTable } from "./db/schema/urls";
+import { encodeBase62 } from "./utils/base62";
+import { eq } from "drizzle-orm";
 
-const app = new Hono();
+type Bindings = {
+  DB: DrizzleD1Database;
+  DOMAIN: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-const api = new Hono().basePath("/api/v1");
+const api = new Hono<{ Bindings: Bindings }>().basePath("/api/v1");
 
 api.post("/urls", async (c) => {
+  // parse the request json body
+  const { longUrl } = await c.req.json();
+
+  const db = drizzle(c.env.DB);
+  const [{ id, createdAt }] = await db
+    .insert(urlsTable)
+    .values({
+      longUrl,
+    })
+    .returning({ id: urlsTable.id, createdAt: urlsTable.createdAt });
+
+  const shortCode = encodeBase62(id);
+  await db.update(urlsTable).set({ shortCode }).where(eq(urlsTable.id, id));
+
+  const domain = c.env.DOMAIN;
   return c.json({
-    shortUrl: "https://yourdomain.com/abc123",
-    shortCode: "abc123",
-    longUrl: "https://example.com/very/long/path",
+    shortUrl: `${domain}/${shortCode}`,
+    shortCode: shortCode,
+    longUrl,
+    createdAt,
   });
 });
 
